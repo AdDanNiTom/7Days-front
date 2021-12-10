2b56fc903fca9e95902e9f17016020424c7867a9
AdrianAranda <adrienaranda@gmail.com>
2021-12-10 12:15:10 +0100

    Added functional marker

--- src/components/Mapbox.jsx
+++ src/components/Mapbox.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import ReactMapGL, { Marker } from "react-map-gl";
 
 function Mapbox() {
   const [viewport, setViewport] = useState({
@@ -11,6 +11,37 @@ function Mapbox() {
     pitch: 15,
   });
 
+  //   const markers = React.useMemo(         ¡¡¡¡¡¡THIS WILL BE IN USE ONCE WE HAVE ALL THE MARKERS!!!!!
+  //     () =>
+  //       data.map((event) => (
+  //         <Marker
+  //           key={event.name}
+  //           longitude={event.longitude}
+  //           latitude={event.latitude}
+  //         >
+  //           <img src="pin.png"  />
+  //         </Marker>
+  //       )),
+  //     [props.data]
+  //   );
+
+  const marker = (
+    <Marker
+      latitude={41.38893706489965}
+      longitude={2.183596863159949}
+      offsetLeft={(-viewport.zoom * 5) / 2}
+      offsetTop={(-viewport.zoom * 5)}
+    >
+      <img
+        src="https://cdn-icons.flaticon.com/png/512/450/premium/450016.png?token=exp=1639129298~hmac=fb00dbdf134306e684187737a4c0b235"
+        alt="location"
+        height={viewport.zoom * 5}
+        width={viewport.zoom * 5}
+      />
+    </Marker>
+  );
+//   const geo = navigator.geolocation.getCurrentPosition()
+
   return (
     <ReactMapGL
       {...viewport}
@@ -18,9 +49,10 @@ function Mapbox() {
       width="100vw"
       height="100vh"
       mapStyle="mapbox://styles/mapbox/streets-v10"
-
       onViewportChange={(viewport) => setViewport(viewport)}
-    />
+    >
+      {marker}
+    </ReactMapGL>
   );
 }
 
