a7717c9584e18cda046b0fb13042cd26323abbb9
AdrianAranda <adrienaranda@gmail.com>
2021-12-14 13:53:56 +0100

    changes to webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
-module.exports = {
+
+
+const config = {
   use: {
     loader: 'babel-loader',
     options: {
       presets: ['my-custom-babel-preset'],
       ignore: [ './node_modules/mapbox-gl/dist/mapbox-gl.js' ]
     }}}
+
+
+
+    module.exports = config
