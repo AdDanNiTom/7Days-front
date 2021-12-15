2bb4d3aa153f74d76c48ba894643191a6595e185
nicomellon <nmellon94@gmail.com>
2021-12-15 17:43:01 +0100

    ready to go

--- src/components/events/SevenDays.jsx
+++ src/components/events/SevenDays.jsx
@@ -3,7 +3,21 @@ import { Pagination } from "react-bootstrap";
 function SevenDays(props) {
   const { parentCb, filterState } = props;
   // days for pagination, currently always shows Sunday first. Work in progress
-  let sevenDays = ["Su", "M", "T", "W", "Th", "F", "S"];
+  // let sevenDays = ["Su", "M", "T", "W", "Th", "F", "S"];
+  const currentDay = new Date().getDay();
+  const sevenDays = [
+    { abbreviation: "Su", index: 0 },
+    { abbreviation: "M", index: 1 },
+    { abbreviation: "T", index: 2 },
+    { abbreviation: "W", index: 3 },
+    { abbreviation: "Th", index: 4 },
+    { abbreviation: "F", index: 5 },
+    { abbreviation: "S", index: 6 },
+  ];
+  const startsToday = sevenDays.slice(currentDay);
+  const endsYesterday = sevenDays.slice(0, currentDay);
+
+  const orderedDays = startsToday.concat(endsYesterday);
 
   return (
     <Pagination>
@@ -15,13 +29,13 @@ function SevenDays(props) {
         ALL
       </Pagination.Item>
 
-      {sevenDays.map((oneDay, index) => (
+      {orderedDays.map((oneDay) => (
         <Pagination.Item
-          onClick={() => parentCb(index, filterState.category)}
-          key={index}
-          active={index === filterState.weekday}
+          onClick={() => parentCb(oneDay.index, filterState.category)}
+          key={oneDay.index}
+          active={oneDay.index === filterState.weekday}
         >
-          {oneDay}
+          {oneDay.abbreviation}
         </Pagination.Item>
       ))}
     </Pagination>
--- src/pages/events/EventsPage.jsx
+++ src/pages/events/EventsPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
 import { Card, Collapse } from "react-bootstrap";
@@ -11,13 +11,12 @@ import * as api from "../../apiRequests.js";
 import Loading from "../../components/status/Loading";
 
 function EventMapMerge(props) {
-  
   const [showFilters, setShowFilters] = useState(false);
   const [filteredEvents, setFilteredEvents] = useState(null);
-  const [hasChanged, setHasChanged] = useState(false)
-  
+  const [hasChanged, setHasChanged] = useState(false);
+
   function refreshCB() {
-    setHasChanged(!hasChanged)
+    setHasChanged(!hasChanged);
   }
 
   // Filtering
@@ -41,7 +40,10 @@ function EventMapMerge(props) {
   const view = new URLSearchParams(search).get("view");
 
   // react-query finds events from selected day
-  const { data, status } = useQuery(["dayEvents", hasChanged ], api.fetchAllEvents);
+  const { data, status } = useQuery(
+    ["dayEvents", hasChanged],
+    api.fetchAllEvents
+  );
 
   useEffect(() => {
     setFilteredEvents(data);
@@ -76,7 +78,11 @@ function EventMapMerge(props) {
       {view === "map" ? (
         <Mapbox events={filteredEvents} />
       ) : (
-        <EventsList refreshCB={refreshCB} events={filteredEvents} />
+        <EventsList
+          refreshCB={refreshCB}
+          events={filteredEvents}
+          filterState={filter}
+        />
       )}
     </>
   );
