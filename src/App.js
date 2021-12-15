4e473638c74644bed5560492a17146e530531f48
nicomellon <nmellon94@gmail.com>
2021-12-15 12:14:56 +0100

    working on good popups

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ import EventDetailsPage from "./pages/EventDetailsPage";
 import EditEventPage from "./pages/EditEventPage";
 import ProfilePage from "./pages/profile/ProfilePage";
 import EditProfilePage from "./pages/profile/EditProfilePage";
-import SignupPage from "./pages/SignupPage";
-// import LoginPage from "./pages/LoginPage";
 import PrivateRoute from "./components/PrivateRoute";    // <== IMPORT
 import AnonRoute from "./components/AnonRoute";        // <== IMPORT
 import Welcome from "./pages/Welcome";
@@ -15,16 +13,22 @@ import EventsPage from "./pages/events/EventsPage";
 
 import ErrorToast from "./components/status/ErrorToast";
 import { AuthContext } from "./context/auth.context";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import SuccessToast from "./components/status/SuccessToast";
 
 function App(props) {
-  const {error, success} = useContext(AuthContext)
+  const {error, setError, success, setSuccess} = useContext(AuthContext)
   const location = useLocation();
+
+  useEffect(() => {
+    setError(false)
+    // setSuccess(false)
+  }, [location])
+
   return (
     <div className="App">
-        <Route exact path="/" component={Welcome} />
       <Switch>      
+        <AnonRoute exact path="/" component={Welcome} />
 
         {/* 👇 UPDATE THE EXISTING ROUTES 👇  */}
         <PrivateRoute exact path="/events" component={EventsPage} />
@@ -32,11 +36,7 @@ function App(props) {
         <PrivateRoute exact path="/events/edit/:id" component={EditEventPage} />
         <PrivateRoute exact path="/profile/edit" component={EditProfilePage} />
         <PrivateRoute exact path="/profile/:id" component={ProfilePage} />
-        {/* <PrivateRoute exact path="/profile" component={ProfilePage} /> */}
         
-        <AnonRoute exact path="/signup" component={SignupPage} />
-        {/* <AnonRoute exact path="/login" component={LoginPage} /> */}
-        {/* <AnonRoute exact path="/test" component={EventsPage} /> */}
 
       </Switch>
       {location.pathname !== '/' && <Navbar />}
